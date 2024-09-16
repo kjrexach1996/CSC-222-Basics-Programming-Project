a32bf635dcb95a731d9098a6eda1e4dc78e92438
Kevin Rexach <kaiorex@hotmail.com>
2024-09-16 10:16:45 -0400

    First part of Basics Progamming Project Assignment with some changed data types and changes to formatting.

--- CSC 222 Basics Programming Project.cpp
+++ CSC 222 Basics Programming Project.cpp
@@ -8,15 +8,16 @@ int main()
 {
     // Initializes variables
     int gallons_in_car = 20;
-    int distance_per_gallon_town = 23.5, distance_per_gallon_highway = 28.9;
+    double distance_per_gallon_town = 23.5, distance_per_gallon_highway = 28.9;
 
     // Calculates total distances car can travel
-    int total_distance_town = gallons_in_car * distance_per_gallon_town;
-    int total_ditance_highway = gallons_in_car * distance_per_gallon_highway;
+    double total_distance_town = gallons_in_car * distance_per_gallon_town;
+    double total_ditance_highway = gallons_in_car * distance_per_gallon_highway;
 
     //Displays total distances
-    cout << "The car can travel", total_distance_town, "in town on one tank of gas.";
-    cout << "The car can travel", total_ditance_highway, "on the highway on one tank of gas.";
+    cout << "The car can travel " << total_distance_town << " miles in town on one tank of gas." << endl;
+    cout << "The car can travel " << total_ditance_highway << " miles on the highway on one tank of gas." << endl;
+    return 0;
 }
 
 
