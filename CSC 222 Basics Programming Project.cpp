5099972dc2e510d354e9811ed3056393b6494b2f
Kevin Rexach <kaiorex@hotmail.com>
2024-09-16 10:06:17 -0400

    First project in Basics  Programming Project assignment

--- CSC 222 Basics Programming Project.cpp
+++ CSC 222 Basics Programming Project.cpp
@@ -1,11 +1,22 @@
-// CSC 222 Basics Programming Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//This program calculates and displays the total number of miles a car with
+//20 gallons of gas can travel on one tank of gas in town and in highway, respectively.
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Initializes variables
+    int gallons_in_car = 20;
+    int distance_per_gallon_town = 23.5, distance_per_gallon_highway = 28.9;
+
+    // Calculates total distances car can travel
+    int total_distance_town = gallons_in_car * distance_per_gallon_town;
+    int total_ditance_highway = gallons_in_car * distance_per_gallon_highway;
+
+    //Displays total distances
+    cout << "The car can travel", total_distance_town, "in town on one tank of gas.";
+    cout << "The car can travel", total_ditance_highway, "on the highway on one tank of gas.";
 }
 
 
