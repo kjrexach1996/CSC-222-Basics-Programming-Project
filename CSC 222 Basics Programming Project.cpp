422a26c4c37960f00e70714b21e005dc1f9f7661
Kevin Rexach <kaiorex@hotmail.com>
2024-09-16 09:39:41 -0400

    Add project files.

---
+++ CSC 222 Basics Programming Project.cpp
@@ -0,0 +1,11 @@
+// CSC 222 Basics Programming Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
