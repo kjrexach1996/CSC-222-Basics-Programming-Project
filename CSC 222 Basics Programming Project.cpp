5189dd8a2163428e54d9da30cd234551313e5a30
Kevin Rexach <kaiorex@hotmail.com>
2024-09-16 11:29:13 -0400

    Second part of Basics Programming Project assignment; changed information being displayed by rearranging lines and adding setprecision manipulator.

--- CSC 222 Basics Programming Project.cpp
+++ CSC 222 Basics Programming Project.cpp
@@ -3,20 +3,21 @@
 //citrus flavor of those who purchase at least one drink a week.
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
     //Initializes variables and calculates different numbers of customers
     int customers_surveyed = 16500;
-    double customers_who_purchase_drinks = customers_surveyed * .15;
+    int customers_who_purchase_drinks = customers_surveyed * .15;
     double customers_who_prefer_citrus = customers_who_purchase_drinks * .58;
 
     // Displays information
-    cout << "The number of customers who purchase at least one energy drink a week is " << customers_who_purchase_drinks << "." << endl;
-    cout << "The number of customers that purchase energy drinks and prefer citrus flavor is " << customers_who_prefer_citrus << "." << endl;
-
-
+    cout << "The approximate number of customers who purchase at least one energy drink a week is " << customers_who_purchase_drinks << "." << endl;
+    cout << "The approximate number of customers that purchase energy drinks and prefer citrus flavor is ";
+    cout << setprecision(4) << customers_who_prefer_citrus << "." << endl;
+    return 0;
 }
 
 
